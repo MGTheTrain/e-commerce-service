42083f1789110b98059863c0d0f80edf809cbf73
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-12 21:31:32 +0200

    replace duplicate

--- backend/Mgtt.ECom/src/Mgtt.ECom.Domain/ShoppingCart/CartItem.cs
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Domain/ShoppingCart/CartItem.cs
@@ -2,30 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Mgtt.ECom.Domain.ShoppingCart
+namespace Mgtt.EComm.Domain.ShoppingCart
 {
     /// <summary>
-    /// Represents a shopping cart in the e-commerce system.
+    /// Represents a shopping cart item in the e-commerce system.
     /// </summary>
-    public class Cart : IValidatableObject
+    public class CartItem : IValidatableObject
     {
-        public int CartID { get; internal set; }
+        public int CartItemID { get; internal set; }
 
         [Required]
-        public int UserID { get; set; }
+        public int CartID { get; set; }
 
         [Required]
-        [Range(0, double.MaxValue, ErrorMessage = "Total amount must be non-negative")]
-        public float TotalAmount { get; set; }
+        public int ProductID { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
+        public float Price { get; set; }
 
         /// <summary>
-        /// Validates the properties of the Cart object.
+        /// Validates the properties of the CartItem object.
         /// </summary>
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (TotalAmount < 0)
+            if (Quantity <= 0)
             {
-                yield return new ValidationResult($"{nameof(TotalAmount)} can't be negative");
+                yield return new ValidationResult($"{nameof(Quantity)} must be greater than zero");
+            }
+
+            if (Price <= 0)
+            {
+                yield return new ValidationResult($"{nameof(Price)} must be greater than zero");
             }
 
             yield return ValidationResult.Success;

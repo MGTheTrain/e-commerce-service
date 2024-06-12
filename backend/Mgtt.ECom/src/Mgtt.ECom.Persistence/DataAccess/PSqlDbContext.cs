bca3626a45dad6343960a695ee23ddcfe5d2f638
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-12 21:52:26 +0200

    implement dbcontext, consider domain models and create initial migration

---
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Persistence/DataAccess/PSqlDbContext.cs
@@ -0,0 +1,90 @@
+﻿using Mgtt.ECom.Domain.OrderManagement;
+using Mgtt.ECom.Domain.ProductManagement;
+using Mgtt.ECom.Domain.ReviewManagement;
+using Mgtt.ECom.Domain.ShoppingCart;
+using Mgtt.ECom.Domain.UserManagement;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mgtt.ECom.Persistence.DataAccess;
+/// <summary>
+/// Represents the PostgreSQL database context
+/// </summary>
+public class PsqlDbContext : DbContext
+{
+    /// <summary>Default constructor<\summary>
+    /// <remark>
+    /// Comment this line out when initially creating first migration with 
+    /// `dotnet-ef migrations add InitialCreate --output-dir Migrations`
+    /// <\remark>
+    public PsqlDbContext(DbContextOptions<PsqlDbContext> options) : base(options)
+    { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+
+    /// <summary>
+    /// Configures the database connection and options.
+    /// </summary>
+    /// <param name="optionsBuilder">Options builder for configuring the DbContext.</param>
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql();
+    }
+
+    /// <summary>
+    /// Configures the entity mappings and relationships.
+    /// </summary>
+    /// <param name="modelBuilder">Model builder for building entity mappings.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>(e =>
+        {
+            e.HasKey(o => o.UserID);
+            e.Property(o => o.UserID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<Order>(e =>
+        {
+            e.HasKey(o => o.OrderID);
+            e.Property(o => o.OrderID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<OrderItem>(e =>
+        {
+            e.HasKey(o => o.OrderItemID);
+            e.Property(o => o.OrderItemID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<Product>(e =>
+        {
+            e.HasKey(o => o.ProductID);
+            e.Property(o => o.ProductID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<Category>(e =>
+        {
+            e.HasKey(o => o.CategoryID);
+            e.Property(o => o.CategoryID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<Review>(e =>
+        {
+            e.HasKey(o => o.ReviewID);
+            e.Property(o => o.ReviewID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<Cart>(e =>
+        {
+            e.HasKey(o => o.CartID);
+            e.Property(o => o.CartID).ValueGeneratedOnAdd();
+        });
+        modelBuilder.Entity<CartItem>(e =>
+        {
+            e.HasKey(o => o.CartItemID);
+            e.Property(o => o.CartItemID).ValueGeneratedOnAdd();
+        });
+    }
+}
---
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Persistence/Migrations/20240612195131_InitialCreate.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Mgtt.ECom.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    CartItemID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CartID = table.Column<int>(type: "integer", nullable: false),
+                    ProductID = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.CartItemID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    CartID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserID = table.Column<int>(type: "integer", nullable: false),
+                    TotalAmount = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.CartID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    OrderItemID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OrderID = table.Column<int>(type: "integer", nullable: false),
+                    ProductID = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.OrderItemID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserID = table.Column<int>(type: "integer", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TotalAmount = table.Column<float>(type: "real", nullable: false),
+                    OrderStatus = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Stock = table.Column<int>(type: "integer", nullable: false),
+                    CategoryID = table.Column<int>(type: "integer", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductID = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false),
+                    Comment = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    ReviewDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

536bef58d3225884eff8cd54b2401b6cfb7cc6bc
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-13 21:45:07 +0200

    validate request dtos

--- backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ProductManagement/Dtos/CategoryRequestDTO.cs
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ProductManagement/Dtos/CategoryRequestDTO.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mgtt.ECom.Web.v1.ProductManagement.DTOs
 {
     public class CategoryRequestDTO
     {
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
         public string Description { get; set; }
--- backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ProductManagement/Dtos/ProductRequestDTO.cs
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ProductManagement/Dtos/ProductRequestDTO.cs
@@ -2,16 +2,24 @@ namespace Mgtt.ECom.Web.v1.ProductManagement.DTOs
 {
     public class ProductRequestDTO
     {
+        [Required]
         public Guid CategoryID { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
         public float Price { get; set; }
 
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be non-negative")]
         public int Stock { get; set; }
 
+        [Required]
         public string ImageUrl { get; set; }
     }
 }

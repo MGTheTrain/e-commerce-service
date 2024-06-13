7ce3056e1157e8e68546591ac221737cec23c3a5
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-13 22:26:56 +0200

    setup Program.cs

--- backend/Mgtt.ECom/src/Mgtt.ECom.Web/Program.cs
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Web/Program.cs
@@ -3,70 +3,64 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "e-commerce-service",
+        Version = "v1",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Marvin Gajek",
+            Email = "marvin.gajek@outlook.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MgTheTrain License",
+            Url = new Uri("https://github.com/MGTheTrain/multi-media-management-service")
+        },
+        Description = @"API documentation for the e-commerce-service",
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Add JWT with Bearer here",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+          Reference = new OpenApiReference
+          {
+              Type = ReferenceType.SecurityScheme,
+              Id = "Bearer"
+          }
+          },
+          new string[] { }
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
-// Add swagger definitions
-// ```cs 
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-//     {
-//         Title = "Mgtt.ECom",
-//         Version = "v1",
-//         TermsOfService = new Uri("https://example.com/terms"),
-//         Contact = new OpenApiContact
-//         {
-//             Name = "Marvin Gajek",
-//             Email = "marvin.gajek@outlook.com"
-//         },
-//         License = new OpenApiLicense
-//         {
-//             Name = "MgTheTrain License",
-//             Url = new Uri("https://github.com/MGTheTrain/multi-media-management-service")
-//         },
-//         Description = @"API documentation for the Mgtt.ECom",
-//     });
-//     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//     {
-//         In = ParameterLocation.Header,
-//         Description = "Add JWT with Bearer here",
-//         Name = "Authorization",
-//         Type = SecuritySchemeType.ApiKey
-//     });
-//     c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-//     {
-//         new OpenApiSecurityScheme
-//         {
-//           Reference = new OpenApiReference
-//           {
-//               Type = ReferenceType.SecurityScheme,
-//               Id = "Bearer"
-//           }
-//           },
-//           new string[] { }
-//         }
-//     });
-//     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-//     c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-// });
-// ```
+var psqlDbconnectionString = builder.Configuration.GetConnectionString("PsqlDatabase");
+var dbContext = builder.Services.AddDbContext<DbContext, PsqlDbContext>(options =>
+{
+    options.UseNpgsql(psqlDbconnectionString);
+});
 
-// Consider dependency injection through appsettings.json files when creating singleton or transient instances, e.g.:
-// ```cs
-// if (builder.Configuration["BlobStorageProvider"]! == "Gcp")
-// {
-//     var gcpBlobConnectorSettings = new GcpBlobConnectorSettings();
-//     gcpBlobConnectorSettings.ConnectionString = builder.Configuration.GetSection("gcpBlobConnectorSettings")["ConnectionString"]!;
-//     ...
-//     builder.Services.AddSingleton<IBlobConnector, GcpBlobConnector>(
-//     x => new GcpBlobConnector(
-//         Options.Create<GcpBlobConnectorSettings>(gcpBlobConnectorSettings))
-//     );
-// }
-// ...
-// builder.Services.AddTransient<IDomainAService, DomainAService>(); // utilizes Db Contexts
-// builder.Services.Singleton<IDomainBService, DomainBService>();
-// ```
+builder.Services.AddTransient<IOrderItemService, OrderItemService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<IReviewService, ReviewService>();
+builder.Services.AddTransient<ICartService, CartService>();
+builder.Services.AddTransient<ICartItemService, CartItemService>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 var app = builder.Build();
 

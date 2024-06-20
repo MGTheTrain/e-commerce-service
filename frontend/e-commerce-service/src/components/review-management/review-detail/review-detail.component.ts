cbb0ea85520a8255daac596008d67726bd38bfff
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-20 22:33:48 +0200

    add components

---
+++ frontend/e-commerce-service/src/components/review-management/review-detail/review-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './review-detail.component.html',
+  styleUrl: './review-detail.component.css'
+})
+export class ReviewDetailComponent {
+
+}

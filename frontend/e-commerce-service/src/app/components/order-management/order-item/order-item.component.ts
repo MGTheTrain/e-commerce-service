e95ed92e137750d10f1e05fdba7fc4347c71ab2f
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 21:45:49 +0200

    consider product response dto in order item component

--- frontend/e-commerce-service/src/app/components/order-management/order-item/order-item.component.ts
+++ frontend/e-commerce-service/src/app/components/order-management/order-item/order-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { OrderItemResponseDTO } from '../../../generated/api';
+import { OrderItemResponseDTO, ProductResponseDTO } from '../../../generated/api';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -18,6 +18,21 @@ export class OrderItemComponent {
     quantity: 0,
     price: 0
   };
+
+  @Input() product: ProductResponseDTO = {
+    productID: '1',
+    categoryID: '1',
+    name: 'Dean Razorback Guitar',
+    description: 'Product Description',
+    price: 99.99,
+    stock: 10,
+    imageUrl: 'https://www.musicconnection.com/wp-content/uploads/2021/01/dean-dime-620x420.jpg'
+  };
+
+  ngOnInit(): void {
+    // GET api/v1/orders/:orderId/item request
+    // GET api/v1/product/:productId
+  }
 
   constructor() { }
 }

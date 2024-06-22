5659b34cc4abab29cfd8779993c49df3da093b9c
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 12:47:54 +0200

    setup user list component with mock data

--- frontend/e-commerce-service/src/app/components/user-management/user-list/user-list.component.ts
+++ frontend/e-commerce-service/src/app/components/user-management/user-list/user-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { UserResponseDTO } from '../../../generated/api';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule, FontAwesomeModule ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
 export class UserListComponent {
+  users: UserResponseDTO[] = [
+    { userID: '1', userName: 'John Doe', email: 'john.doe@example.com', role: 'Admin' },
+    { userID: '2', userName: 'Jane Smith', email: 'jane.smith@example.com', role: 'User' },
+    { userID: '3', userName: 'Mike Johnson', email: 'mike.johnson@example.com', role: 'User' },
+    { userID: '4', userName: 'Emily Brown', email: 'emily.brown@example.com', role: 'Admin' },
+    { userID: '5', userName: 'Chris Lee', email: 'chris.lee@example.com', role: 'User' },
+  ];
+
+  public faSearch: IconDefinition = faSearch;
+  public searchText: string = '';
+
+  filteredUsers() {
+    return this.users.filter(user => {
+      return (
+        Object.values(user).some(val => 
+          typeof val === 'string' && val.toLowerCase().includes(this.searchText.toLowerCase())
+        )
+      );
+    });
+  }
 
 }

af6a280f758cabdd075c332c5636ed3df68ce3b4
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 21:15:56 +0200

    consider product response dto in cart item component

--- frontend/e-commerce-service/src/app/components/shopping-cart/cart-item/cart-item.component.ts
+++ frontend/e-commerce-service/src/app/components/shopping-cart/cart-item/cart-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { CartItemResponseDTO } from '../../../generated/api';
+import { CartItemResponseDTO, ProductResponseDTO } from '../../../generated/api';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -18,6 +18,21 @@ export class CartItemComponent {
     quantity: 2,
     price: 25.0
   };
+
+  @Input() product: ProductResponseDTO = {
+    productID: '1',
+    categoryID: '1',
+    name: 'Product Name',
+    description: 'Product Description',
+    price: 99.99,
+    stock: 10,
+    imageUrl: 'https://www.musicconnection.com/wp-content/uploads/2021/01/dean-dime-620x420.jpg'
+  };
+
+  ngOnInit(): void {
+    // GET api/v1/cart/:cartId/item request
+    // GET api/v1/product/:productId
+  }
 
   onUpdateQuantity(newQuantity: number): void {
     this.cartItem.quantity = newQuantity;

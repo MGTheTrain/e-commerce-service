72941a2826d21861610132c751ede861e40d83cc
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 16:36:21 +0200

    setup user login component

--- frontend/e-commerce-service/src/app/components/user-management/user-login/user-login.component.ts
+++ frontend/e-commerce-service/src/app/components/user-management/user-login/user-login.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faApple, faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-login',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule, FontAwesomeModule ],
   templateUrl: './user-login.component.html',
   styleUrl: './user-login.component.css'
 })
 export class UserLoginComponent {
+  email: string = '';
+  password: string = '';
+  public faSignInAlt: IconDefinition = faSignInAlt;
+  public faMicrosoft: IconDefinition = faMicrosoft;
+  public faApple: IconDefinition = faApple;
+  public faGoogle: IconDefinition = faGoogle;
 
+  onLogin(): void {
+    console.log('Logging in with', this.email, this.password);
+  }
+
+  onSSOLogin(provider: string): void {
+    console.log('Logging in with', provider);
+  }
+
+  onForgotPassword(): void {
+    console.log('Redirecting to forgot password page');
+  }
+
+  onRegister(): void {
+    console.log('Redirecting to register page');
+  }
 }

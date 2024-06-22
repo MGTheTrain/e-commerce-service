d2d10d2680b6e5b3e860c2daa79d56979b871020
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 23:21:40 +0200

    ensure 1 cart belongs to 1 user

--- frontend/e-commerce-service/src/app/components/shopping-cart/cart/cart.component.ts
+++ frontend/e-commerce-service/src/app/components/shopping-cart/cart/cart.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { CartItemResponseDTO, CartResponseDTO } from '../../../generated/api';
+import { Component, Input } from '@angular/core';
+import { CartItemResponseDTO, CartResponseDTO, ProductResponseDTO } from '../../../generated/api';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { CartItemComponent } from '../cart-item/cart-item.component';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -12,16 +14,40 @@ import { CartItemComponent } from '../cart-item/cart-item.component';
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-  cart: CartResponseDTO = {
+  private subscription: Subscription | null = null;
+
+  @Input() cart: CartResponseDTO = {
     cartID: 'cart1',
     userID: 'user1',
     totalAmount: 50.0
   };
 
-  cartItems: CartItemResponseDTO[] = [
+  @Input() cartItems: CartItemResponseDTO[] = [
     { cartItemID: '1', cartID: 'cart1', productID: 'product1', quantity: 2, price: 25.0 },
     { cartItemID: '2', cartID: 'cart1', productID: 'product2', quantity: 1, price: 20.0 }
   ];
+
+  @Input() product: ProductResponseDTO = {
+    productID: '1',
+    categoryID: '1',
+    name: 'Dean Razorback Guitar',
+    description: 'Product Description',
+    price: 99.99,
+    stock: 10,
+    imageUrl: 'https://www.musicconnection.com/wp-content/uploads/2021/01/dean-dime-620x420.jpg'
+  };
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      let id = params['userId'];
+      this.cart.userID = id;
+
+      // GET /api/v1/cart/:userId/items
+      // GET /api/v1/products/:productId
+   });
+  }
 
   onUpdateCart(): void {
     console.log('Updating cart:', this.cart);

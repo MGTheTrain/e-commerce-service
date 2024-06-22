ed56f26dc3a9017cdc862409e93b655ab539fa0e
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 14:26:27 +0200

    setup review detail component with mock data

--- frontend/e-commerce-service/src/app/components/review-management/review-detail/review-detail.component.ts
+++ frontend/e-commerce-service/src/app/components/review-management/review-detail/review-detail.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ReviewResponseDTO } from '../../../generated/api';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-review-detail',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule, FontAwesomeModule ],
   templateUrl: './review-detail.component.html',
-  styleUrl: './review-detail.component.css'
+  styleUrls: ['./review-detail.component.css']
 })
 export class ReviewDetailComponent {
+  private subscription: Subscription | null = null;
 
+  @Input() review: ReviewResponseDTO = {
+    reviewID: '1',
+    productID: '1',
+    userID: 'user1',
+    rating: 4,
+    comment: 'Great product!',
+    reviewDate: new Date('2023-06-01')
+  };
+  
+  public faTrash: IconDefinition = faTrash;
+  public faEdit: IconDefinition = faEdit;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      let id = params['reviewId'];
+      this.review.reviewID = id;
+   });
+  }
+
+  onDelete(): void {    
+    console.log('Deleting review:', this.review);    
+  }
+
+  onUpdate(): void {    
+    console.log('Updating review:', this.review);    
+  }
 }

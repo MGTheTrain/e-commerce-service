d8c420ac65de8e030cfe3d27200c8dbb775f3080
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 14:22:46 +0200

    setup product detail component with mock data

--- frontend/e-commerce-service/src/app/components/product-management/product-detail/product-detail.component.ts
+++ frontend/e-commerce-service/src/app/components/product-management/product-detail/product-detail.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { ProductResponseDTO } from '../../../generated/api';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule, FontAwesomeModule ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+  private subscription: Subscription | null = null;
 
+  product: ProductResponseDTO = {
+    productID: '1',
+    categoryID: '1',
+    name: 'Product Name',
+    description: 'Product Description',
+    price: 99.99,
+    stock: 10,
+    imageUrl: 'path-to-image.jpg'
+  };
+
+  public faTrash: IconDefinition = faTrash;
+  public faEdit: IconDefinition = faEdit;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      let id = params['productId'];
+      this.product.productID = id;
+   });
+  }
+
+  onDelete() {
+    console.log('Deleting product with ID:', this.product.productID);
+  }
+
+  onUpdate() {
+    console.log('Updating product:', this.product);
+  }
 }

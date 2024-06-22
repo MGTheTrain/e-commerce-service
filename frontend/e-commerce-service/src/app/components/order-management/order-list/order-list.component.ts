7741edc691f442dd43b3b13860af13f35e9ffb9d
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-22 12:16:26 +0200

    utilize swagger-generated response dto

--- frontend/e-commerce-service/src/app/components/order-management/order-list/order-list.component.ts
+++ frontend/e-commerce-service/src/app/components/order-management/order-list/order-list.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { OrderResponseDTO } from '../../../generated/api';
 
 @Component({
   selector: 'app-order-list',
@@ -13,22 +14,23 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './order-list.component.css'
 })
 export class OrderListComponent {
+  orders: OrderResponseDTO[] = [
+    { orderID: '1', userID: '101', orderDate: new Date('2023-06-01'), totalAmount: 250.75, orderStatus: 'pending' },
+    { orderID: '2', userID: '102', orderDate: new Date('2023-06-10'), totalAmount: 150.50, orderStatus: 'shipped' },
+    { orderID: '3', userID: '103', orderDate: new Date('2023-06-15'), totalAmount: 300.00, orderStatus: 'delivered' },
+    { orderID: '4', userID: '104', orderDate: new Date('2023-06-20'), totalAmount: 100.99, orderStatus: 'pending' },
+    { orderID: '5', userID: '105', orderDate: new Date('2023-06-25'), totalAmount: 200.20, orderStatus: 'shipped' },
+  ];
+
   public faSearch: IconDefinition = faSearch;
   public searchText: string = '';
   public filterOption: string = '';
-  public orders = [
-    { name: 'Order 1', status: 'pending' },
-    { name: 'Order 2', status: 'shipped' },
-    { name: 'Order 3', status: 'delivered' },
-    { name: 'Order 4', status: 'pending' },
-    { name: 'Order 5', status: 'shipped' },
-  ];
 
   filteredOrders() {
     return this.orders.filter(order => {
       return (
-        (!this.searchText || order.name.toLowerCase().includes(this.searchText.toLowerCase())) &&
-        (!this.filterOption || order.status === this.filterOption)
+        (!this.searchText || order.orderID?.includes(this.searchText) || order.orderStatus?.toLowerCase().includes(this.searchText.toLowerCase())) &&
+        (!this.filterOption || order.orderStatus === this.filterOption)
       );
     });
   }

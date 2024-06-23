ab8376241c54f5648ef2f391db5bfb35ae82b744
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-23 13:18:56 +0200

    modify order detail component

--- frontend/e-commerce-service/src/app/components/order-management/order-detail/order-detail.component.ts
+++ frontend/e-commerce-service/src/app/components/order-management/order-detail/order-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { OrderItemResponseDTO, OrderResponseDTO } from '../../../generated';
+import { Component, Input, OnInit } from '@angular/core';
+import { OrderItemResponseDTO, OrderResponseDTO, UserResponseDTO } from '../../../generated';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
@@ -8,26 +8,34 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { OrderItemComponent } from '../order-item/order-item.component';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-order-detail',
   standalone: true,
-  imports: [ FormsModule, CommonModule, FontAwesomeModule, OrderItemComponent ],
+  imports: [FormsModule, CommonModule, FontAwesomeModule, OrderItemComponent],
   templateUrl: './order-detail.component.html',
   styleUrls: ['./order-detail.component.css']
 })
-export class OrderDetailComponent {
+export class OrderDetailComponent implements OnInit {
   private subscription: Subscription | null = null;
-  
+
+  @Input() user: UserResponseDTO = { 
+    userID: '1', 
+    userName: 'John Doe', 
+    email: 'john.doe@example.com', 
+    role: 'Admin' 
+  };
+
   @Input() order: OrderResponseDTO = {
-    orderID: '1',
-    userID: 'user1',
+    orderID: uuidv4(),
+    userID: '1',
     orderDate: new Date('2023-06-01'),
-    totalAmount: 250.75,
+    totalAmount: 0,
     orderStatus: 'pending'
   };
 
-  orderItems: OrderItemResponseDTO[] = [
+  @Input() orderItems: OrderItemResponseDTO[] = [
     { orderItemID: '1', orderID: '1', productID: '1', quantity: 2, price: 100 },
     { orderItemID: '2', orderID: '1', productID: '2', quantity: 1, price: 50 }
   ];
@@ -41,7 +49,16 @@ export class OrderDetailComponent {
     this.subscription = this.route.params.subscribe(params => {
       let id = params['orderId'];
       this.order.orderID = id;
-   });
+    });
+    this.calculateTotalAmount();
+  }
+
+  calculateTotalAmount(): void {
+    this.order.totalAmount = this.orderItems.reduce((total, item) => {
+      const quantity = item.quantity ?? 0;
+      const price = item.price ?? 0;
+      return total + (quantity * price);
+    }, 0);
   }
 
   onDelete(): void {    

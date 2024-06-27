5878aaa89fac3234e9e0524af8a27d3e5cda7b21
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-27 21:24:11 +0200

    add navigation from review list to review creation component

--- frontend/e-commerce-service/src/app/components/review-management/review-list/review-list.component.ts
+++ frontend/e-commerce-service/src/app/components/review-management/review-list/review-list.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProductResponseDTO, ReviewResponseDTO, ReviewService, UserResponseDTO } from '../../../generated';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { v4 as uuidv4 } from 'uuid';
 import { HeaderComponent } from '../../header/header.component';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-review-list',
   standalone: true,
-  imports: [ CommonModule, FormsModule, HeaderComponent ],
+  imports: [ CommonModule, FormsModule, FontAwesomeModule, HeaderComponent ],
   templateUrl: './review-list.component.html',
   styleUrl: './review-list.component.css'
 })
 export class ReviewListComponent {
-  reviews: ReviewResponseDTO[] = [
+  @Input() reviews: ReviewResponseDTO[] = [
     { reviewID: uuidv4(), productID: '1', userID: '1', rating: 4, comment: 'Great product!', reviewDate: new Date('2023-06-01') },
     { reviewID: uuidv4(), productID: '4', userID: '2', rating: 5, comment: 'Excellent service!', reviewDate: new Date('2023-06-02') },
     { reviewID: uuidv4(), productID: '2', userID: '3', rating: 3, comment: 'Could be better.', reviewDate: new Date('2023-06-03') },
@@ -22,7 +25,7 @@ export class ReviewListComponent {
     { reviewID: uuidv4(), productID: '2', userID: '5', rating: 4, comment: 'Good value for money.', reviewDate: new Date('2023-06-05') },
   ];
 
-  users: UserResponseDTO[] = [
+  @Input() users: UserResponseDTO[] = [
     { userID: '1', userName: 'John Doe', email: 'john.doe@example.com', role: 'Admin' },
     { userID: '2', userName: 'Jane Smith', email: 'jane.smith@example.com', role: 'User' },
     { userID: '3', userName: 'Mike Johnson', email: 'mike.johnson@example.com', role: 'User' },
@@ -30,13 +33,15 @@ export class ReviewListComponent {
     { userID: '5', userName: 'Chris Lee', email: 'chris.lee@example.com', role: 'User' },
   ];
 
-  products: ProductResponseDTO[] = [
+  @Input() products: ProductResponseDTO[] = [
     { productID: '1', categoryID: '101', name: 'Dean Razorback Guitar Blue', description: 'Description of Product A', price: 4999.99, stock: 10, imageUrl: 'https://s.yimg.com/ny/api/res/1.2/jVphTvtt1LwM3foboVcs_w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02MDA-/https://media.zenfs.com/en-US/homerun/consequence_of_sound_458/830585263f74148d1ac63c91bfe6e2f4' },
     { productID: '2', categoryID: '102', name: 'Dean Razorback Guitar White', description: 'Description of Product B', price: 2999.99, stock: 5, imageUrl: 'https://www.musicconnection.com/wp-content/uploads/2021/01/dean-dime-620x420.jpg' },
     { productID: '3', categoryID: '101', name: 'ESP LTD Greeny Black', description: 'Description of Product C', price: 3999.99, stock: 15, imageUrl: 'https://media.sound-service.eu/Artikelbilder/Shopsystem/1200x837/ALEXI%20LAIHO%20GREENY_1.jpg' },
     { productID: '4', categoryID: '103', name: 'Jackson Guitar Blue', description: 'Description of Product D', price: 1099.99, stock: 2, imageUrl: 'https://m.media-amazon.com/images/I/51EnIfqKY7L.jpg' },
     { productID: '5', categoryID: '102', name: 'Gibson Les Paul Black', description: 'Description of Product E', price: 999.99, stock: 8, imageUrl: 'https://morningsideschoolofmusic.co.uk/wp-content/uploads/2022/05/Gibson-Guitars-1024x576.jpg' },
   ];
+
+  public faPlus: IconDefinition = faPlus;
 
   constructor(private router: Router, private reviewService: ReviewService) {}
 
@@ -49,6 +54,10 @@ export class ReviewListComponent {
         console.error('Error fetching reviews', error);
       }
     );
+  }
+
+  handleCreateReviewClick(): void {
+    this.router.navigate(['/reviews/creation']);
   }
 
   getUserName(userID: string | undefined): string | undefined {

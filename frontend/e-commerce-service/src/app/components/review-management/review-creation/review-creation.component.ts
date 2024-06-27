c79c666a615ead680a0b7c5fd1b20b65f63f3c57
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-27 21:00:18 +0200

    modify review creation component

--- frontend/e-commerce-service/src/app/components/review-management/review-creation/review-creation.component.ts
+++ frontend/e-commerce-service/src/app/components/review-management/review-creation/review-creation.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faArrowLeft, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { ProductResponseDTO, ReviewResponseDTO, UserResponseDTO } from '../../../generated';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DetailHeaderComponent } from '../../header/detail-header/detail-header.component';
 
 @Component({
   selector: 'app-review-creation',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, FontAwesomeModule, DetailHeaderComponent],
   templateUrl: './review-creation.component.html',
   styleUrl: './review-creation.component.css'
 })
 export class ReviewCreationComponent {
+  @Input() review: ReviewResponseDTO = {
+    reviewID: '1',
+    productID: '1',
+    userID: 'user1',
+    rating: 4,
+    comment: 'Great product!',
+    reviewDate: new Date('2023-06-01')
+  };
 
+  @Input() user: UserResponseDTO = { 
+    userID: '1', 
+    userName: 'John Doe', 
+    email: 'john.doe@example.com', 
+    role: 'Admin' 
+  };
+
+  @Input() product: ProductResponseDTO = { 
+    productID: '1', 
+    categoryID: '101', 
+    name: 'Dean Razorback Guitar Blue', 
+    description: 'Description of Product A', 
+    price: 4999.99, 
+    stock: 10, 
+    imageUrl: 'https://s.yimg.com/ny/api/res/1.2/jVphTvtt1LwM3foboVcs_w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD02MDA-/https://media.zenfs.com/en-US/homerun/consequence_of_sound_458/830585263f74148d1ac63c91bfe6e2f4' 
+  };
+  
+  public faPlus: IconDefinition = faPlus;
+  public faArrowLeft: IconDefinition = faArrowLeft;
+
+  constructor(private router: Router) { }
+
+  handleNavigateBackClick(): void {
+    this.router.navigate(['/reviews']);
+  }
+
+  handleCreateReviewClick(): void {
+    console.log('Creating review:', this.review);    
+  }
 }

48e423707e5a88ff8d3d965aa14922bc97303eeb
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-30 15:36:11 +0200

    modify routes

--- frontend/e-commerce-service/src/app/app.routes.ts
+++ frontend/e-commerce-service/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/error-pages/page-not-found/page-not-found.component';
-import { UserLoginComponent } from './components/user-management/user-login/user-login.component';
-import { UserRegistrationComponent } from './components/user-management/user-registration/user-registration.component';
+// import { UserLoginComponent } from './components/user-management/user-login/user-login.component';
+// import { UserRegistrationComponent } from './components/user-management/user-registration/user-registration.component';
+// import { ResetPasswordComponent } from './components/user-management/reset-password/reset-password.component';
 import { UserProfileComponent } from './components/user-management/user-profile/user-profile.component';
 import { ProductListComponent } from './components/product-management/product-list/product-list.component';
 import { ProductDetailComponent } from './components/product-management/product-detail/product-detail.component';
@@ -12,19 +13,18 @@ import { CartComponent } from './components/shopping-cart/cart/cart.component';
 import { ReviewListComponent } from './components/review-management/review-list/review-list.component';
 import { ReviewDetailComponent } from './components/review-management/review-detail/review-detail.component';
 import { UserListComponent } from './components/user-management/user-list/user-list.component';
-import { ResetPasswordComponent } from './components/user-management/reset-password/reset-password.component';
 import { CartListComponent } from './components/shopping-cart/cart-list/cart-list.component';
 import { ProductCreationComponent } from './components/product-management/product-creation/product-creation.component';
 import { ReviewCreationComponent } from './components/review-management/review-creation/review-creation.component';
 
 const routes: Routes = [
-    { path: 'user/registration', component: UserRegistrationComponent },
-    { path: 'user/login', component: UserLoginComponent },
-    { path: 'user/reset-password', component: ResetPasswordComponent },
+    // { path: 'user/registration', component: UserRegistrationComponent },
+    // { path: 'user/login', component: UserLoginComponent },
+    // { path: 'user/reset-password', component: ResetPasswordComponent },
     { path: 'user/:userId', component: UserProfileComponent },
     { path: 'user/:userId/cart', component: CartComponent },
     { path: 'user', component: UserListComponent },
-    { path: 'products', component: ProductListComponent },
+    { path: '', component: ProductListComponent },
     { path: 'products/creation', component: ProductCreationComponent },
     { path: 'products/:productId', component: ProductDetailComponent },
     { path: 'orders', component: OrderListComponent },
@@ -34,7 +34,6 @@ const routes: Routes = [
     { path: 'reviews', component: ReviewListComponent },
     { path: 'reviews/creation', component: ReviewCreationComponent },
     { path: 'reviews/:reviewId', component: ReviewDetailComponent },
-    { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route redirects to product list
     { path: '**', component: PageNotFoundComponent },
 ];
 

53559752b3228e429b5dceab5a47594ca60c6ca6
Marvin Gajek <Marvin.Gajek1@web.de>
2024-06-30 16:30:34 +0200

    update audience

--- frontend/e-commerce-service/environments/environment.ts
+++ frontend/e-commerce-service/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth0: {
     domain: 'dev-d81cx3ar5jc0hn2z.us.auth0.com',
     clientId: 'dD1q7SCw77VapBOv86a320PLIYKKrdmu',
-    audience: 'https://quickstart/api',
+    audience: 'https://ecom/api',
   },
   internalWebApiBasePath: 'http://localhost:5000'
 };

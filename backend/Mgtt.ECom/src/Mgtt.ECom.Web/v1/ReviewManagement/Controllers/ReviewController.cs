12ec928e4e09e87737527db4fc9bba94252ec693
Marvin Gajek <Marvin.Gajek1@web.de>
2024-07-06 21:01:25 +0200

    add helper method in case permission claims equals manage:own-review

--- backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ReviewManagement/Controllers/ReviewController.cs
+++ backend/Mgtt.ECom/src/Mgtt.ECom.Web/v1/ReviewManagement/Controllers/ReviewController.cs
@@ -6,13 +6,12 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
 {
     using System;
     using System.Collections.Generic;
+    using System.Security.Claims;
     using System.Threading.Tasks;
     using Mgtt.ECom.Domain.ReviewManagement;
     using Mgtt.ECom.Web.V1.ReviewManagement.DTOs;
-    using Mgtt.ECom.Web.V1.UserManagement.DTOs;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using static System.Collections.Specialized.BitVector32;
 
     [Route("api/v1/reviews")]
     [ApiController]
@@ -26,22 +25,59 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         }
 
         /// <summary>
+        /// Checks if the user has either "manage:reviews" or "manage:own-review" permission and validates their reviews if applicable.
+        /// </summary>
+        /// <returns>True if the user has the permission and valid reviews; otherwise, false.</returns>
+        private async Task<bool> CheckManageOwnReviewPermission()
+        {
+            var permissionsClaim = this.User.FindFirst("permissions");
+            if(permissionsClaim!.Value.Split(' ').Contains("manage:reviews"))
+            {
+                return true;
+            }
+            else if (permissionsClaim!.Value.Split(' ').Contains("manage:own-review"))
+            {
+                var userIdClaim = this.User.FindFirst(ClaimTypes.NameIdentifier);
+                if (userIdClaim != null)
+                {
+                    var userId = Guid.Parse(userIdClaim.Value);
+                    var userReviews = await this.reviewService.GetReviewsByUserId(userId);
+                    if (userReviews != null)
+                    {
+                        return true;
+                    }
+                }
+            } 
+            return false;
+        }
+
+        /// <summary>
         /// Creates a new review.
         /// </summary>
         /// <param name="reviewDTO">The review data transfer object containing review details.</param>
         /// <returns>The newly created review.</returns>
         /// <response code="201">Returns the newly created review.</response>
         /// <response code="400">If the review data is invalid.</response>
+        /// <response code="401">If the user is not authenticated.</response>
+        /// <response code="403">If the user is not allowed to manage the resource.</response>
         [HttpPost]
         [Authorize("manage:reviews")]
         [Authorize("manage:own-review")]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ReviewResponseDTO))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<IActionResult> CreateReview(ReviewRequestDTO reviewDTO)
         {
             if (!this.ModelState.IsValid)
             {
                 return this.BadRequest(this.ModelState);
+            }
+
+            var checkManageOwnReviewPermission = await CheckManageOwnReviewPermission();
+            if(!checkManageOwnReviewPermission)
+            {
+                return Forbid();
             }
 
             var review = new Review
@@ -161,35 +197,6 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         }
 
         /// <summary>
-        /// Retrieves reviews by user ID.
-        /// </summary>
-        /// <param name="userId">The ID of the user.</param>
-        /// <returns>A list of reviews by the specified user.</returns>
-        /// <response code="200">Returns a list of reviews by the specified user.</response>
-        [HttpGet("user/{userId}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<ReviewResponseDTO>))]
-        public async Task<ActionResult<IEnumerable<ReviewResponseDTO>>> GetReviewsByUserId(Guid userId)
-        {
-            var reviews = await this.reviewService.GetReviewsByUserId(userId);
-            var reviewDTOs = new List<ReviewResponseDTO>();
-
-            foreach (var review in reviews)
-            {
-                reviewDTOs.Add(new ReviewResponseDTO
-                {
-                    ReviewID = review.ReviewID,
-                    ProductID = review.ProductID,
-                    UserID = review.UserID,
-                    Rating = review.Rating,
-                    Comment = review.Comment,
-                    ReviewDate = review.ReviewDate,
-                });
-            }
-
-            return this.Ok(reviewDTOs);
-        }
-
-        /// <summary>
         /// Updates an existing review.
         /// </summary>
         /// <param name="reviewId">The ID of the review to update.</param>
@@ -197,6 +204,8 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         /// <response code="204">If the review was successfully updated.</response>
         /// <response code="400">If the review data is invalid.</response>
         /// <response code="404">If the review is not found.</response>
+        /// <response code="401">If the user is not authenticated.</response>
+        /// <response code="403">If the user is not allowed to manage the resource.</response>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpPut("{reviewId}")]
         [Authorize("manage:reviews")]
@@ -204,11 +213,19 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ReviewResponseDTO))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public async Task<IActionResult> UpdateReview(Guid reviewId, ReviewRequestDTO reviewDTO)
         {
             if (!this.ModelState.IsValid)
             {
                 return this.BadRequest(this.ModelState);
+            }
+
+            var checkManageOwnReviewPermission = await CheckManageOwnReviewPermission();
+            if(!checkManageOwnReviewPermission)
+            {
+                return Forbid();
             }
 
             var review = await this.reviewService.GetReviewById(reviewId);
@@ -248,6 +265,8 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         /// <param name="reviewId">The ID of the review to delete.</param>
         /// <response code="204">If the review was successfully deleted.</response>
         /// <response code="404">If the review is not found.</response>
+        /// <response code="401">If the user is not authenticated.</response>
+        /// <response code="403">If the user is not allowed to manage the resource.</response>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         [HttpDelete("{reviewId}")]
         [Authorize("manage:reviews")]
@@ -256,6 +275,12 @@ namespace Mgtt.ECom.Web.V1.ReviewManagement.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> DeleteReview(Guid reviewId)
         {
+            var checkManageOwnReviewPermission = await CheckManageOwnReviewPermission();
+            if(!checkManageOwnReviewPermission)
+            {
+                return Forbid();
+            }
+
             var review = await this.reviewService.GetReviewById(reviewId);
 
             if (review == null)
